65afcb4618f4aaa57989fccb8d86fd96b1dc6152
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-29 14:03:48 -0500

    add test api

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // connect to DB
-mongoose.connect(process.env.MONGO_URL).then(() => console.log("DBConnection Successfull"
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("DBConnection Successful"
 )).catch((err) => {
    console.log(err);
 });
+
+// create REST ENDPOINTS
+app.get("/api/test", () => {
+  console.log("test is successful")
+})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running")
