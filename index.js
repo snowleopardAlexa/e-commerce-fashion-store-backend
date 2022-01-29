a8707687e25130b6d6a848d241dd880976ccc79a
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-29 13:53:53 -0500

    connect index.js to db mongodb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello2")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+// connect to DB
+mongoose.connect(
+    "mongodb+srv://alexa:Valencia19@e-commerce-fashion-stor.fsbmt.mongodb.net/CruelaDeVil?retryWrites=true&w=majority"
+).then(() => console.log("DBConnection Successfull"
+)).catch((err) => {
+   console.log(err);
+});
+
+app.listen(5000, () => {
+    console.log("Backend server is running")
+})
