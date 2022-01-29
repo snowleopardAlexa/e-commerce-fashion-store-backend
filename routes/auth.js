f7ff797cd6f0cc3d2c41031999ecd9008ccf7e41
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-29 14:48:30 -0500

    add try and catch block and async await function

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,21 @@
 const router = require("express").Router();
-const User = require("../models/User")
+const User = require("../models/User");
 
-// REGISTER 
-router.post("/register", (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    });
+// REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-   // save info to DB
-   const savedUser = newUser.save();
-   console.log(savedUser);
-
+  try {
+    // save info to DB
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
